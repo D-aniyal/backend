a48884f71d96f5119371b9b99e4d63f132a0bbea
D-aniyal <daniyalnaeem5@gmail.com>
2019-03-12 17:33:29 +0400

    Add files via upload

--- PlexoGaming/routes/web.php
+++ PlexoGaming/routes/web.php
@@ -10,10 +10,25 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+/**/
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Auth::routes();
 
-Route::get('/', 'PagesController@index');
+Route::get('signup', 'UserController@signup');
+Route::post('signup', 'UserController@register');
+
+Route::get('login', 'UserController@login')->name('login');
+Route::post('login', 'UserController@authenticate');
+
+Route::get('logout', 'UserController@logout')->name('logout');
+
+Route::get('profile', 'ProfileController@profile');
+Route::post('profile', 'ProfileController@save_profile');
+
+Route::get('generalprofile', 'ProfileController@generalprofile');
+Route::post('generalprofile', 'ProfileController@save_generalprofile');
 
 Route::get('/support', 'PagesController@support');
 Route::get('/findmatch', 'PagesController@findmatch');
@@ -22,7 +37,6 @@ Route::get('/leaderboard', 'PagesController@leaderboard');
 Route::get('/wager', 'PagesController@wager');
 Route::get('/tournament', 'PagesController@tournament');
 Route::get('/bank', 'PagesController@bank');
-Route::get('/profile', 'PagesController@profile');
 Route::get('/match', 'PagesController@match');
 Route::get('/viewteam', 'PagesController@viewteam');
 Route::get('/viewteam1', 'PagesController@viewteam1');
@@ -37,10 +51,19 @@ Route::get('/notification', 'PagesController@notification');
 Route::get('/selectionslider', 'PagesController@selectionslider');
 Route::get('/submittmatch', 'PagesController@submittmatch');
 Route::get('/Tournament1', 'PagesController@Tournament1');
+
 Route::get('/generalprofile', 'PagesController@generalprofile');
 
 Route::get('/mockedlogin', 'PagesController@mockedlogin');
 
 Route::resource('posts', 'PostsController');
+Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('profile', 'ProfileController@profile');
+Route::post('/addProfile', 'ProfileController@addProfile');
+
+
+
+Route::get('/gamecategory', 'GamecategoryController@gamecategory');
